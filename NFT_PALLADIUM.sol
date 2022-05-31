2391a1fd8e23a03deb49c8ddb478c741e5e9cf4c
AlbertoHortaBtc <40526601+albertohortabtc@users.noreply.github.com>
2022-05-30 21:21:02 -0500

    Update NFT_PALLADIUM.sol

--- NFT_PALLADIUM.sol
+++ NFT_PALLADIUM.sol
@@ -22,7 +22,7 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract PALLADIUM is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, EIP712, ERC721Votes {
     using Counters for Counters.Counter;
-    uint256 maxSup = 50;
+    uint256 maxSup = 51;
 
     Counters.Counter private _tokenIdCounter;
 
